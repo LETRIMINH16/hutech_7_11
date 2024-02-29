f160af3a6fd7bd1840c1e3c4a0fadfee5603ce65
LE TRI MINH <lgsmin14@gmail.com>
2024-02-29 15:21:25 +0700

    first cm

---
+++ student.java
@@ -0,0 +1,18 @@
+public class student{
+    //Props
+    private String id;
+    private String name;
+    private String add;
+
+    //methods
+    public String getname(){
+        return name;
+    }
+    public String getid(){
+        return id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("stop");
+    }
+}
